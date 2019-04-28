8eef18811e6423952c92e15d836fdfe7e4e09a5e
Evil <lordcashtugbaski@gmail.com>
2019-04-28 16:40:44 +0100

    Removed Upload Files

--- backend/Use.js
+++
@@ -1,9 +0,0 @@
-const {resolve} = require('path');
-
-module.exports = {
-    // Use Middlewares
-    middlewares: {
-        'Auth': $.backendPath('/middle/AuthMiddleware'),
-        // 'Formidable': '//xjs-formidable-middleware'
-    }
-};
--- backend/routers/web.js
+++ backend/routers/web.js
@@ -1,6 +1,1 @@
 const Route = $.router;
-
-Route.path('/upload', ()=>{
-    Route.get('', 'getUpload');
-    Route.post('', 'postUpload');
-}).controller('Upload').actionsAsName().as('upload');
